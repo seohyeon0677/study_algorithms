e64750dc324410ea121448526f39676bd2c8d9e9
Seohyeon Lee <seohyeon0677@gmail.com>
2024-12-27 11:18:22 +0900

    [fix] bj 16944

--- bj/bj_16955.py
+++ bj/bj_16955.py
@@ -1,51 +1,43 @@
 # X가 승리할 수 있으면 1, 아니면 0
 
-# xx.xx
-# xxxx.
-# .xxxx
+def play(board, r, c):
+    directions = [
+        (0,1),
+        (1,0),
+        (1,1),
+        (1,-1)
+    ]
 
-def play(game):
-    # 가로
-    for i in range(10):
-        for j in range(6):
-            if  game[i][j] == 'X' and game[i][j+1] == 'X' and game[i][j+2] == 'X' and game[i][j+3] == 'X' and game[i][j+4] == '.':
-                return 1
-            if  game[i][j] == '.' and game[i][j+1] == 'X' and game[i][j+2] == 'X' and game[i][j+3] == 'X' and game[i][j+4] == 'X':
-                return 1
-            if  game[i][j] == 'X' and game[i][j+1] == 'X' and game[i][j+2] == '.' and game[i][j+3] == 'X' and game[i][j+4] == 'X':
-                return 1
-    
-    # 세로
-    for i in range(6):
-        for j in range(10):
-            if game[i][j] == 'X' and game[i+1][j] == 'X' and game[i+2][j] == 'X' and game[i+3][j] == 'X' and game[i+4][j] == '.':
-                return 1
-            if game[i][j] == '.' and game[i+1][j] == 'X' and game[i+2][j] == 'X' and game[i+3][j] == 'X' and game[i+4][j] == 'X':
-                return 1
-            if game[i][j] == 'X' and game[i+1][j] == 'X' and game[i+2][j] == '.' and game[i+3][j] == 'X' and game[i+4][j] == 'X':
-                return 1
+    for dr, dc in directions:
+        count = 1
 
-    # 우하향 대각선
-    for i in range(6):
-        for j in range(6):
-            if game[i][j] == 'X' and game[i+1][j+1] == 'X' and game[i+2][j+2] == 'X' and game[i+3][j+3] == 'X' and game[i+4][j+4] == '.':
-                return 1
-            if game[i][j] == '.' and game[i+1][j+1] == 'X' and game[i+2][j+2] == 'X' and game[i+3][j+3] == 'X' and game[i+4][j+4] == 'X':
-                return 1
-            if game[i][j] == 'X' and game[i+1][j+1] == 'X' and game[i+2][j+2] == '.' and game[i+3][j+3] == 'X' and game[i+4][j+4] == 'X':
-                return 1
+        for i in range(1,5):
+            nr, nc = r + dr * i, c + dc * i
+            if 0 <= nr < 10 and 0 <= nc < 10 and board[nr][nc] == 'X':
+                count += 1
+            else:
+                break
 
-    # 좌하향 대각선선
-    for i in range(5, 10):
-        for j in range(5, 10):
-            if game[i][j] == 'X' and game[i-1][j-1] == 'X' and game[i-2][j-2] == 'X' and game[i-3][j-3] == 'X' and game[i-4][j-4] == '.':
-                return 1
-            if game[i][j] == '.' and game[i-1][j-1] == 'X' and game[i-2][j-2] == 'X' and game[i-3][j-3] == 'X' and game[i-4][j-4] == 'X':
-                return 1
-            if game[i][j] == 'X' and game[i-1][j-1] == 'X' and game[i-2][j-2] == '.' and game[i-3][j-3] == 'X' and game[i-4][j-4] == 'X':
-                return 1
-    
+        for i in range(1,5):
+            nr, nc = r - dr * i, c - dc * i
+            if 0 <= nr < 10 and 0 <= nc < 10 and board[nr][nc] == 'X':
+                count += 1
+            else:
+                break
+
+        if count >= 5:
+            return True
+    return False
+
+def win(board):
+    for r in range(10):
+        for c in range(10):
+            if board[r][c] == '.':
+                board[r] = board[r][:c] + 'X' + board[r][c+1:]
+                if play(board, r, c):
+                    return 1
+                board[r] = board[r][:c] + '.' + board[r][c+1:]
     return 0
 
-game = [input().strip() for _ in range(10)]
-print(play(game))
+board = [input().strip() for _ in range(10)]
+print(win(board))
