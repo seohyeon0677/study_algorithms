5a0e42cf5741fe8d8aaabb643b17a8cfece85316
Seohyeon Lee <seohyeon0677@gmail.com>
2025-01-01 20:23:43 +0900

    [fix] bj 10250

--- bj/bj_10250.py
+++ bj/bj_10250.py
@@ -3,11 +3,12 @@ tc = [list(map(int, input().split())) for _ in range(T)]
 
 for i in range(T):
     H, W, N = tc[i][0], tc[i][1], tc[i][2]  # H(층), W(방), N(손님)
-    n = N // H + 1
-    f = N % H
+    n = (N - 1) // H + 1
+    f = H if N % H == 0 else N % H
 
     if n < 10:
-        print(f"{f}0{n}")
+        room = int(str(f) + '0' + str(n))
     else:
-        print(f"{f}{n}")
+        room = int(str(f) + str(n))
     
+    print(room)
