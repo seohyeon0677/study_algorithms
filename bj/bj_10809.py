90fda0f52e8ee4a93069bcc91046d830857d2baa
Seohyeon Lee <seohyeon0677@gmail.com>
2025-01-03 10:39:39 +0900

    [re] bj 10809

--- bj/bj_10809.py
+++ bj/bj_10809.py
@@ -1,14 +1,8 @@
-def counts(S, a):
-    for i in range(len(S)):
-        if S[i] == a:
-            return i
-    return -1
-
-S = input()
+S = input().strip()
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 answer = []
 for char in alphabet:
-    answer.append(counts(S, char))
+    answer.append(S.find(char))
 
 print(' '.join(map(str, answer)))
