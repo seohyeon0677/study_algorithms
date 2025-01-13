50133253f6e2c1d701f10a8e1572060dff96acd2
Seohyeon Lee <seohyeon0677@gmail.com>
2025-01-13 12:38:09 +0900

    [fix] bj 7568

--- bj/bj_7568.py
+++ bj/bj_7568.py
@@ -1,27 +1,12 @@
-# (몸무게 x, 키 y)
 N = int(input())
-M = [list(map(int, input().split())) for _ in range(N)]
+M = [tuple(map(int, input().split())) for _ in range(N)]
 
-answer = [N] * N
-for a in range(0, N-1):
-    for b in range(a+1, N):
-        if a != b:
-            if M[a][0] > M[b][0] and M[a][1] > M[b][1]:
-                answer[a] -= 1
-            elif M[a][0] > M[b][0] and M[a][1] > M[b][1]:
-                answer[b] -= 1
-            else:
-                continue
+ranks = [1] * N
 
-rank = [0] * N
-n = 1
-while n <= N:
-    for m in range(1, N+1):
-        count = 0
-        for i in range(N):
-            if answer[i] == m:
-                count += 1
-                rank[i] = n
-        n += count
+for i in range(N):
+    for j in range(N):
+        if i != j:
+            if M[i][0] < M[j][0] and M[i][1] < M [j][1]:
+                ranks[i] += 1
 
-print(*rank)
+print(" ".join(map(str, ranks)))
