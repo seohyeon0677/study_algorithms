a879b890700aced2c463c3428611ad43be9d61a2
Seohyeon Lee <seohyeon0677@gmail.com>
2025-01-13 23:18:53 +0900

    [re] bj 15650

--- bj/bj_15650.py
+++ bj/bj_15650.py
@@ -1,14 +1,13 @@
-def choice(N, M, current):
+def choice(start, N, M, current):
     if len(current) == M:
         print(" ".join(map(str, current)))
         return
     
-    for i in range(1, N + 1):
-        if not current or i > current[-1]:
-            current.append(i)
-            choice(N, M, current)
-            current.pop()
+    for i in range(start, N + 1):
+        current.append(i)
+        choice(i + 1, N, M, current)
+        current.pop()
 
 N, M = map(int, input().split())
 
-choice(N, M, [])
+choice(1, N, M, [])
